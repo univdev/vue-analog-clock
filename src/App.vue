28348d2a71be54130642523c6417c4fb3480d1dd
박찬영 <pcydeveloper@gmail.com>
2022-03-31 09:56:00 +0900

    chore: Vite + Vue 설치

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+.container
+  clock-component
+</template>
+
+<script lang="ts" setup>
+import ClockComponent from './components/Clock.vue';
+</script>
---
+++ src/components/Clock.vue
@@ -0,0 +1,11 @@
+<style lang="less" scoped>
+
+</style>
+
+<template lang="pug">
+.clock asd
+</template>
+
+<script lang="ts" setup>
+
+</script>
