56562f81e42fbab48ccf7b3a8d0a67eace4a1d26
박찬영 <pcydeveloper@gmail.com>
2022-04-05 00:24:46 +0900

    feat: 현재 시간으로 Vuex 초기화

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,46 @@
+<style lang="less" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+</style>
+
 <template lang="pug">
 .container
-  clock-component
+  clock-component(
+    :date="current")
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
+import { computed, ComputedRef, defineComponent, onBeforeMount, onBeforeUnmount } from 'vue';
+import { useStore } from 'vuex';
 import ClockComponent from './components/Clock.vue';
+
+export default defineComponent({
+  components: {
+    ClockComponent,
+  },
+  setup() {
+    const store = useStore();
+    const second = 1000;
+    const current: ComputedRef<Date> = computed(() => store.state.app.time);
+    const initializeCurrentTime = () => {
+      const date = new Date();
+      store.commit('app/SET_TIME', date);
+    };
+    let setStartTimer = null;
+    onBeforeMount(() => {
+      initializeCurrentTime();
+      setStartTimer = window.setInterval(initializeCurrentTime, second);
+    });
+    onBeforeUnmount(() => {
+      window.clearInterval(setStartTimer);
+    });
+    return {
+      current,
+    };
+  },
+});
 </script>
