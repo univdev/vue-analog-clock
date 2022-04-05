9a7b42143c2221e8a7c15e430895f111f330bbce
박찬영 <pcydeveloper@gmail.com>
2022-04-05 13:52:51 +0900

    feat: common.less 전역 파일 추가 및 적용

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import Store from './store';
+import './assets/common/styles/common.less';
+import 'animate.css';
 
 const app = createApp(App);
 
